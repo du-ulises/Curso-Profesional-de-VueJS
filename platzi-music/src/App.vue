609c64daf76d4b56e1fa6ac91bec6fe395b6599c
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-18 20:59:01 -0500

    Ejercicio de SASS y PUG

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -1,7 +1,27 @@
 <template lang="pug">
   #app
-    img(src="./assets/logo.png")
-    h1 {{ msg }}
+    .columns
+      .column
+      .column.is-three-fifths
+        img(src="./assets/logo.png")
+        h1 Registro Estación Platzi
+        br
+        .field
+          label.label Nombre
+            .control
+              input.input(type="text", name='nombre', placeholder="Nombre")
+        .field
+          label.label Apellidos
+            .control
+              input.input(type="text", name='apellidos', placeholder="Apellidos")
+        .field
+          label.label Cargo
+            .control
+              input.input(type="text", name='cargo', placeholder="Cargo")
+        .field.is-grouped.is-grouped-right
+          .control
+            button.button.is-primary Enviar
+      .column
 </template>
 
 <script>
@@ -17,4 +37,29 @@ export default {
 
 <style lang="scss">
   @import './scss/main.scss';
+  #app {
+    margin-top: 5%;
+    margin-left: 5%;
+    max-width: 420px;
+  }
+  h1 {
+    color: #A3ECF3;
+    font-size: 20px;
+    font-weight: bolder;
+    text-align: center
+  }
+  img {
+    margin:10px auto;
+		display:block;
+  }
+  .label{
+    color:#FF9300;
+    font-size: 11px;
+  }
+  .input{
+    border: none;
+    border-bottom: 1px solid #FF9300;
+    box-shadow: none !important;
+    color:#212121;
+  }
 </style>
