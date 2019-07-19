5e0ed2ae2140588629b96d785a9a78e3f7fd6def
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-18 21:10:50 -0500

    Ejercicio Avanzado de Pug y SASS

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -1,36 +1,25 @@
 <template lang="pug">
   #app
-    .columns
-      .column
-      .column.is-three-fifths
-        img(src="./assets/logo.png")
-        h1 Registro Estación Platzi
-        br
-        .field
-          label.label Nombre
-            .control
-              input.input(type="text", name='nombre', placeholder="Nombre")
-        .field
-          label.label Apellidos
-            .control
-              input.input(type="text", name='apellidos', placeholder="Apellidos")
-        .field
-          label.label Cargo
-            .control
-              input.input(type="text", name='cargo', placeholder="Cargo")
-        .field.is-grouped.is-grouped-right
-          .control
-            button.button.is-primary Enviar
-      .column
+    search
+    sideBar
+    movieCards
 </template>
 
 <script>
+import Search from'./components/search/Search.vue'
+import SideBar from'./components/sideBar/SideBar.vue'
+import movieCards from'./components/movieCards/movieCards.vue'
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Hello World'
     }
+  },
+  components: {
+    Search,
+    SideBar,
+    movieCards
   }
 }
 </script>
@@ -38,28 +27,8 @@ export default {
 <style lang="scss">
   @import './scss/main.scss';
   #app {
-    margin-top: 5%;
-    margin-left: 5%;
-    max-width: 420px;
-  }
-  h1 {
-    color: #A3ECF3;
-    font-size: 20px;
-    font-weight: bolder;
-    text-align: center
-  }
-  img {
-    margin:10px auto;
-		display:block;
-  }
-  .label{
-    color:#FF9300;
-    font-size: 11px;
-  }
-  .input{
-    border: none;
-    border-bottom: 1px solid #FF9300;
-    box-shadow: none !important;
-    color:#212121;
-  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
---
+++ platzi-music/src/components/movieCards/movieCards.vue
@@ -0,0 +1,92 @@
+<template lang='pug'>
+  #movieCards
+    div.movie-card-container
+      h1 Available movies
+      div.movie-cards
+        div.movie-card(v-for='movie in movies')
+          img(src='../../assets/logo.png')
+          div.text-container
+            h2 {{ movie.name }}
+            p {{ movie.desc }}
+</template>
+<script>
+export default {
+  name: 'movieCards',
+  data () {
+    return {
+      movies: [
+        {
+          name: 'Skeleton Dance by Silly Symphonies',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        },
+        {
+          name: 'Skeleton Dance by Silly Symphonies',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        },
+        {
+          name: 'Skeleton Dance by Silly Symphonies',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        },
+        {
+          name: 'Skeleton Dance by Silly Symphonies',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        },
+        {
+          name: 'Skeleton Dance by Silly Symphonies',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        },
+        {
+          name: 'Skeleton Dance by Silly Symphonies',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang='scss'>
+#movieCards {
+  width: 100%;
+}
+
+.movie-card-container {
+  display: flex;
+  flex-direction: column;
+  margin: 20px00119px;
+
+  .movie-card,
+  .movie-cardimg {
+    width: 287px;
+    height: 192px;
+    img {
+      position: absolute;
+      z-index: 0;
+    }
+  }
+
+  .movie-cards {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .movie-card {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    margin: 10px10px10px0;
+
+    .text-container {
+      color: #fff;
+      padding: 3px5px0;
+      z-index: 1;
+      background: #ff535b92;
+      h2 {
+        font-weight: bold;
+      }
+      p {
+        font-size: 12px;
+      }
+    }
+  }
+}
+</style>
