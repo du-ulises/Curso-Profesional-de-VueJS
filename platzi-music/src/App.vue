970ad290f67ed1e8254b6643272114e5bbf565ae
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-07-30 21:59:30 -0500

    Manipulación del DOM

--- platzi-music/src/App.vue
+++ platzi-music/src/App.vue
@@ -1,34 +1,58 @@
 <template lang="pug">
   #app
-    search
-    sideBar
-    movieCards
+    section.section
+      nav.nav.has-shadow
+        .container
+          input.input.is-large(
+            type="text", 
+            placeholder="Buscar canciones",
+            v-model="searchQuery"
+          )
+          a.button.is-info.is-large(@click="search") Buscar
+          a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
+
+      .container.results
+        .columns
+          .columns(v-for="t in tracks" :key="t.id") {{ t.name }} - {{ t.artist }}
+    
 </template>
 
 <script>
-import Search from'./components/search/Search.vue'
-import SideBar from'./components/sideBar/SideBar.vue'
-import movieCards from'./components/movieCards/movieCards.vue'
+const tracks = [
+  { name: "Muchacha", artist: "Luis Alberto Spinetta" },
+  { name: "Baila conmigo", artist: "Davit" },
+  { name: "I want to break free", artist: "Quen" }
+];
+
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Hello World'
+      searchQuery: "",
+      tracks: []
+    };
+  },
+
+  computed: {
+    searchMessage() {
+      return `Encontrados: ${this.tracks.length}`;
     }
   },
-  components: {
-    Search,
-    SideBar,
-    movieCards
+
+  methods: {
+    search() {
+      this.tracks = tracks;
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-  @import './scss/main.scss';
-  #app {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+@import "./scss/main.scss";
+
+.results {
+  margin-top: 50px;
 }
 </style>
