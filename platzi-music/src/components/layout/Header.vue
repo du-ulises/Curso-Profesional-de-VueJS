420c30f9cb9fef536d221f880988a4a92f0bdeff
Diego Ulises Martínez Aguilar <diegoulisesmartinezaguilar@gmail.com>
2019-08-17 18:16:40 -0500

    Creación de componentes

---
+++ platzi-music/src/components/layout/Header.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+    section.hero.is-success
+        .hero-head
+            header.nav
+                .container
+                    .nav-left
+                        .nav-item
+                            strong Platzi Music
+                    .nav-right.nav-menu
+        .hero-body
+            .container.has-text-centered
+                h1.title Platzi Music
+                h2.subtitle Canciones desde el Curso Profesional de Vue       
+</template>
